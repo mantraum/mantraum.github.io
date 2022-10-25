c9df3126fd87c8bdc845b87f206fc3b06eb25e69
만트라움 <mantraum@naver.com>
2022-10-25 17:33:43 +0900

    Update js001.js

--- js/js001.js
+++ js/js001.js
@@ -986,7 +986,7 @@ function draw05() {
 	ctx.fillStyle = "rgba(128, 0, 0,1)"; //색상지정
 	// 변수 tell_e 재사용
 	tell_e = (tell_e-tell_s)/2;
-	ctx.fillText("&#9800;", 300+275*Math.cos(Math.PI*2*tell_s+Math.PI*2*tell_e), 300+275*Math.sin(Math.PI*2*tell_s+Math.PI*2*tell_e));// ARIES
+	ctx.fillText(&#9800;, 300+275*Math.cos(Math.PI*2*tell_s+Math.PI*2*tell_e), 300+275*Math.sin(Math.PI*2*tell_s+Math.PI*2*tell_e));// ARIES
 
 
 	//황소자리 4월 20일 ~ 5월 20일
@@ -1242,10 +1242,10 @@ function draw05() {
 	// 외곽선
 	ctx.beginPath();
 	ctx.arc(300, 300, 260, 0, Math.PI*2, true);
-//	ctx.stroke();
-//
-//	ctx.beginPath();
-//	ctx.arc(300, 300, 290, 0, Math.PI*2, true);
+	ctx.stroke();
+
+	ctx.beginPath();
+	ctx.arc(300, 300, 290, 0, Math.PI*2, true);
 	//ctx.fillStyle = "#E0E4CD";
 	ctx.shadowOffsetX = 10;
 	ctx.shadowOffsetY = 10;
